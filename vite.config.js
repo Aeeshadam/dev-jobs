53524af239ddcd087a45718f7150e177aba0d93f
Aishat Agbolade <aishaagbolade@gmail.com>
2024-05-17 16:04:39 +0100

    removed unused imports

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 
 dotenv.config();
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
