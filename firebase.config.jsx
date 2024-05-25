78c7c43cfdf2a065d47260e2f4249911e4e6d782
Aishat Agbolade <aishaagbolade@gmail.com>
2024-05-25 20:51:02 +0100

    removed meta.env

--- firebase.config.jsx
+++ firebase.config.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_CONFIG_API_KEY,
+  apiKey: process.env.VITE_FIREBASE_CONFIG_API_KEY,
   authDomain: "dev-jobs-b684a.firebaseapp.com",
   projectId: "dev-jobs-b684a",
   storageBucket: "dev-jobs-b684a.appspot.com",
